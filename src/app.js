5052c43b107b356a7df2633d9eab85b948c617de
Petercode <ndubuisipeter7@gmail.com>
2025-12-19 12:40:14 +0000

    style changes

--- src/app.js
+++ src/app.js
@@ -1292,8 +1292,8 @@ const roundRect = (ctx, x, y, width, height, radius) => {
 
   if (view === 'chat') {
     return (
-      <div className="container">
-        <div className="app-container">
+      <div className="container fullscreen-parent">
+        <div className="app-container fullscreen">
           <div className="chat-header">
             <button onClick={goBack} className="back-button-small">
               ←
